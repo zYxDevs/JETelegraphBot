397cd60525a47a3631d6b46d27ff3caa41c584ee
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-03-16 13:01:59 +0530

    Update jebot.py

--- jebot.py
+++ jebot.py
@@ -101,6 +101,18 @@ async def telegraph(client, message):
     finally:
         os.remove(download_location)
 
+@Jebot.on_callback_query()
+async def button(bot, update):
+      cb_data = update.data
+      if "help" in cb_data:
+        await update.message.delete()
+        await help(bot, update.message)
+      elif "about" in cb_data:
+        await update.message.delete()
+        await about(bot, update.message)
+      elif "start" in cb_data:
+        await update.message.delete()
+        await start(bot, update.message)
 
 print(
     """
