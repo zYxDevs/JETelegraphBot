15ceed7095759cadb97cd4c44b78cf78fea5cffe
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-03-16 12:58:11 +0530

    Update jebot.py

--- jebot.py
+++ jebot.py
@@ -3,7 +3,7 @@ import logging
 from pyrogram import Client, filters
 from telegraph import upload_file
 from config import Config
-
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 Jebot = Client(
    "Telegraph Uploader",
@@ -12,17 +12,91 @@ Jebot = Client(
    bot_token=Config.TG_BOT_TOKEN,
 )
 
-@Jebot.on_message(filters.photo)
+@Jebot.on_message(filters.command("start"))
+async def start(client, message):
+   if message.chat.type == 'private':
+       await Jebot.send_message(
+               chat_id=message.chat.id,
+               text="""<b>Hey There, I'm Telegraph Bot
+
+I can upload photos or videos to telegraph. Made by @ImJanindu 🇱🇰
+
+Hit help button to find out more about how to use me</b>""",   
+                            reply_markup=InlineKeyboardMarkup(
+                                [[
+                                        InlineKeyboardButton(
+                                            "Help", callback_data="help"),
+                                        InlineKeyboardButton(
+                                            "Channel", url="https://t.me/Infinity_BOTs")
+                                    ],[
+                                      InlineKeyboardButton(
+                                            "Source Code", url="https://github.com/ImJanindu/JETelegraphBot")
+                                    ]]
+                            ),        
+            disable_web_page_preview=True,        
+            parse_mode="html")
+
+@Jebot.on_message(filters.command("help"))
+async def help(client, message):
+    if message.chat.type == 'private':   
+        await Jebot.send_message(
+               chat_id=message.chat.id,
+               text="""<b>Telegraph Bot Help!
+
+Just send a photo or video less than 5mb file size, I'll upload it to telegraph.
+
+~ @Infinity_BOTs</b>""",
+        reply_markup=InlineKeyboardMarkup(
+                                [[
+                                        InlineKeyboardButton(
+                                            "Back", callback_data="start"),
+                                        InlineKeyboardButton(
+                                            "About", callback_data="about"),
+                                  ],[
+                                        InlineKeyboardButton(
+                                            "Source Code", url="https://github.com/ImJanindu/JETelegraphBot")
+                                    ]]
+                            ),        
+            disable_web_page_preview=True,        
+            parse_mode="html")
+
+@Jebot.on_message(filters.command("about"))
+async def about(client, message):
+    if message.chat.type == 'private':   
+        await Jebot.send_message(
+               chat_id=message.chat.id,
+               text="""<b>About Telegraph Bot!</b>
+
+<b>♞ Developer:</b> <a href="https://t.me/ImJanindu">Janindu 🇱🇰</a>
+
+<b>♞ Support:</b> <a href="https://t.me/InfinityBOTs_Support">Infinity BOTs Support</a>
+
+<b>♞ Library:</b> <a href="https://github.com/pyrogram/pyrogram">Pyrogram</a>
+
+<b>~ @Infinity_BOTs</b>""",
+     reply_markup=InlineKeyboardMarkup(
+                                [[
+                                        InlineKeyboardButton(
+                                            "Back", callback_data="help"),
+                                        InlineKeyboardButton(
+                                            "Source Code", url="https://github.com/ImJanindu/JETelegraphBot")
+                                    ]]
+                            ),        
+            disable_web_page_preview=True,        
+            parse_mode="html")
+
+@Jebot.on_message(filters.photo & filters.video)
 async def telegraph(client, message): 
-    msg = await message.reply_text("Uploading to telegraph...")
+    if message.chat.type == 'private':
+    msg = await message.reply_text("Uploading To Telegraph...")
     download_location = await client.download_media(
         message=message, file_name='root/jetg')
     try:
         response = upload_file(download_location)
-    except Exception as document:
-        await message.reply(message, text=document)
+    except:
+        await msg.edit_text("Upto 5mb file size only supported!") 
     else:
-        await msg.edit_text(f'**Link: https://telegra.ph{response[0]}**',
+        await msg.edit_text(f'**Uploaded To Telegraph 🤗\n\n👉 https://telegra.ph{response[0]}**',
             disable_web_page_preview=True,
         )
     finally:
